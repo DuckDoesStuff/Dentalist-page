24dc96dd91fdcdc89cf2e47b6570aef24ce12c15
DuckDoesStuff <mailtienduc@gmail.com>
2022-09-20 23:07:57 +0700

    Added reuseable service cards

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './Body.css'
 import Banner from './Banner'
 import MainText from './MainText'
+import ServiceCard from './ServiceCard'
 
 const Body = () => {
   return (
     <div className='Body'>
         <Banner />
         <MainText />
+        <ServiceCard />
     </div>
   )
 }
---
+++ src/components/ServiceCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './ServiceCard.css'
+
+const ServiceCard = () => {
+  return (
+    <div className='Card'>
+      <div className="Inner">
+      </div>
+      <div className="Text">
+        <h1>Dentures</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga</p>
+      </div>
+    </div>
+  )
+}
+
+export default ServiceCard
